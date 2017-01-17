368601fa9d82545536e626bed5362137dbfdb017
Kyle Macey <kylemacey@github.com>
2017-01-17 14:08:06 -0500

    Initial commit

---
+++ lib/text_decorator.rb
@@ -0,0 +1,5 @@
+require "text_decorator/version"
+
+module TextDecorator
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'text_decorator'
+
+require 'minitest/autorun'
